94fa353bea5e4418e89a13e8183934f4ca25b959
priyanka0815 <pt290030@gmail.com>
2024-06-15 17:40:36 +0530

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "rollup";
+import typescript from '@rollup/plugin-typescript'
+
+
+
+export default defineConfig({
+    input: "src/index.ts",
+    output:{
+        dir: "dist",
+        format: "es",
+        name: "priyanka-ui"
+    },
+    external: ["react", "react-dom"],
+    plugins: [typescript({ tsconfig: "tsconfig.json"})],
+})
